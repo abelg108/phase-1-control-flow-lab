07de16708532177699f347d62966359e7a460508
Abel Bokure <abel.ngt@gmail.com>
2022-06-15 21:52:13 -0400

    commit tree 3855423a5254ae257955626c2dc204c7a106250d

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride<=400){return 'This one is on me!' }  
+  if(ride >400 && ride <=2000){
+    return 'That will be twenty bucks.'
+ }
+ if(ride>2000 && ride<=2500) {return "I will gladly take your thirty bucks."}
+ else if(ride > 2500)  { return "No can do."}
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+const destination = (city === "NYC" ) ? "Ok, sounds good." : "No go."
+  return destination;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generous){
   // Write your code here!
+  let tip = generous 
+
+  let response
+
+  switch (tip) { 
+    case 'generous':
+       return response = 'Thank you so much.';
+         break;
+
+         case 'not as generous':
+         return response = 'Thank you.';
+         break;
+
+        default :
+      return 'Bye.'
+    break; } 
 }
